0b2f48d31b359dae1c157582358264a1638a25eb
Qunhao S <qunhaosong@gmail.com>
2017-02-21 21:29:24 -0600

    Added proxy

--- flask_app.py
+++ flask_app.py
@@ -4,23 +4,33 @@ from flask import Flask
 import twilio.twiml
 from twilio.rest import TwilioRestClient
 
+import os
+from urlparse import urlparse
+from twilio.rest.resources import Connection
+from twilio.rest.resources.connection import PROXY_TYPE_HTTP
+
+host, port = urlparse(os.environ["http_proxy"]).netloc.split(":")
+Connection.set_proxy_info(
+    host,
+    int(port),
+    proxy_type=PROXY_TYPE_HTTP,
+)
+
 # Twilio account info
-account_sid = "AC11ba____________________96e87003"
-auth_token = "6789333____________________849d2"
-account_num = "+1617_____42"
+account_sid = "AC________________________"
+auth_token = "9_______________"
+account_num = "+141___88"
 client = TwilioRestClient(account_sid, auth_token)
 
 # api.ai account info
-CLIENT_ACCESS_TOKEN = "78c0e0________________fbcd9404a2"
+CLIENT_ACCESS_TOKEN = "_______________"
 ai = apiai.ApiAI(CLIENT_ACCESS_TOKEN)
 
 app = Flask(__name__)
 
-
 @app.route('/hello')
 def hello_world():
     return 'Hello api.ai (from Flask!)'
-
 
 @app.route("/", methods=['GET', 'POST'])
 def server():
@@ -31,7 +41,6 @@ def server():
     # get SMS metadata
     msg_from = request.values.get("From", None)
     msg = request.values.get("Body", None)
-
     # prepare API.ai request
     req = ai.text_request()
     req.lang = 'en'  # optional, default value equal 'en'
@@ -45,7 +54,6 @@ def server():
         response = response_obj["result"]["fulfillment"]["speech"]
         # send SMS response back via twilio
         client.messages.create(to=msg_from, from_=account_num, body=response)
-
     return str(resp)
 
 if __name__ == "__main__":
